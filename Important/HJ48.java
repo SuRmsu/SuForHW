cdc91378e584e32143d278be0016de66040f55dd
SuRmsu <surmsu001@gmail.com>
2023-02-06 19:10:36 +0800

    HJ48 删除单链表中指定位置的链表

---
+++ Important/HJ48.java
@@ -0,0 +1,29 @@
+package Important;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class HJ48 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int count = sc.nextInt();
+            LinkedList<Integer> linkedList = new LinkedList<Integer>();
+            linkedList.add(sc.nextInt());
+            for (int i = 0; i < count - 1; i++) {
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                linkedList.add(linkedList.indexOf(b) + 1,a);
+            }
+            linkedList.remove((linkedList.indexOf(sc.nextInt())));
+
+            Iterator<Integer> it = linkedList.iterator();
+            while (it.hasNext()) {
+                System.out.print(it.next() + " ");
+            }
+            System.out.println(linkedList);
+
+        }
+    }
+}
