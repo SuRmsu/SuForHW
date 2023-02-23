f3653981b9e6208c72bf773dfbed21863c98ea94
SuRmsu <surmsu001@gmail.com>
2023-02-23 11:20:02 +0800

    LC982 划分等大小的k个子集

--- newod/case1/dp/LC698.java
+++ newod/case1/dp/LC698.java
@@ -3,6 +3,9 @@ package newod.case1.dp;
 import java.util.Arrays;
 import java.util.Collections;
 
+/**
+ * 给定一个整数数组  nums 和一个正整数 k，找出是否有可能把这个数组分成 k 个非空子集，其总和都相等。
+ */
 public class LC698 {
     public static void main(String[] args) {
         int[] nums1 = {2, 2, 2, 2, 3, 4, 5};//{4, 3, 2, 3, 5, 2, 1};
